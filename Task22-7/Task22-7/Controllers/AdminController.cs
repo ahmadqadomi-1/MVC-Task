3a7b0668960bcdf4b72f50f97070848d8209f33b
ahmadqadomi-1 <qadomia95@gmail.com>
2024-07-22 20:13:44 +0300

    Task 22-7

---
+++ Task22-7/Task22-7/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task22_7.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult ContactUS()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task22-7/Task22-7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Task22_7.MvcApplication" Language="C#" %>
